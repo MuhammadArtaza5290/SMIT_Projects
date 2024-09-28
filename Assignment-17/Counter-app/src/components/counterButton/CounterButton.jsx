f4d521fa1a00e334dc39a792d84503c2638cee68
MuhammadArtaza5290 <hafizartaza098@gmail.com>
2024-09-28 12:17:54 +0500

    Final commit of counter app

--- Assignment-17/Counter-app/src/components/counterButton/CounterButton.jsx
+++ Assignment-17/Counter-app/src/components/counterButton/CounterButton.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import {increment, decrement, reset} from '../../featuree/counterSlice/counterSlice'
+import "./CounterButton.css"
 function CounterButton() {
     const dispatch = useDispatch();
   return (
     <>
-       <div>
-        <button onClick={() => dispatch(decrement())}>-</button>
-        <button onClick={() => dispatch(increment())}>+</button>   {/* The dispatch sends an action (in this case, increment) to the Redux store.  */}
+       <div className='btn-sec1'>
+        <button onClick={() => dispatch(decrement())} className='btn-decrement'>-</button>
+        <button onClick={() => dispatch(increment())} className='btn-increment'>+</button>   {/* The dispatch sends an action (in this case, increment) to the Redux store.  */}
         </div>
-        <div>
-            <button onClick={() => dispatch(reset())}>Reset</button>
+        <div className='btn-sec2'>
+            <button onClick={() => dispatch(reset())} className='btn-reset'>Reset</button>
         </div>
     </>
   )
--- Assignment-17/Counter-app/src/components/counterDisplay/CounterDisplay.jsx
+++ Assignment-17/Counter-app/src/components/counterDisplay/CounterDisplay.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import "./CounterDisplay.css"
 function CounterDisplay() {
     const count = useSelector((e) => e.counter.value); // As the state in the store changes, useSelector in CounterDisplay.js detects the change and updates the displayed value automatically.
   return (
     <>
     <div><h1>Counter App</h1></div> {/*now counter is incremented by one (in case of increment otherwise use others). */}
-    <div><h1>{count}</h1></div>
+    <div className='counting'><h2>{count}</h2></div>
     </>
   )
 }
